2ca8dcba6ddd65e1017edd24133aa3b0eec7a566
Lukasz <intergenixyt@gmail.com>
2026-07-17 11:06:35 +0200

    not kept

--- lib/first_screen.dart
+++ lib/first_screen.dart
@@ -11,20 +11,71 @@ class FirstScreen extends StatelessWidget {
         backgroundColor: Colors.blue.shade100,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('This is the First Screen'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Go Back'),
-            ),
-          ],
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return _Element(index: index);
+          },
         ),
       ),
+    );
+  }
+}
+
+class _Element extends StatefulWidget {
+  const _Element({
+    super.key,
+    required this.index,
+  });
+
+  final int index;
+
+  @override
+  State<_Element> createState() => _ElementState();
+}
+
+class _ElementState extends State<_Element> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('init ${widget.index}');
+  }
+
+  @override
+  void dispose() {
+    print('dispose ${widget.index}');
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8,
+            vertical: 16,
+          ),
+          child: Row(
+            children: [
+              Text(
+                '${widget.index}: $_counter',
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {
+                  _counter++;
+                  setState(() {});
+                },
+                icon: Icon(Icons.plus_one),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
